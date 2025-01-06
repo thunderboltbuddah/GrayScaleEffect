e80572c2d3462aa91cfbf5ba48fb06bea8627969
thunderboltbuddah <aownmuhammad070@gmail.com>
2025-01-07 02:31:31 +0500

    Added Output sample image

--- public/main.js
+++ public/main.js
@@ -2,13 +2,13 @@ async function init() {
   let rustApp = null
 
   try {
-    rustApp = await import('../pkg/')
+    rustApp = await import('../pkg')
   } catch(err) {
     console.error(err)
     return;
   }
 
-  
+  console.log(rustApp)
 
   const input = document.getElementById('upload')
   const fileReader = new FileReader()
@@ -17,11 +17,10 @@ async function init() {
     let base64 = fileReader.result.replace(
       /^data:image\/(png|jpeg|jpg);base64,/, ''
     )
-    console.log("before rust")
-   let img_data_url=rustApp.grayscale(base64)
-   document.getElementById('new-img').setAttribute(
-    'src',img_data_url
-   )
+    let img_data_url = rustApp.grayscale(base64)
+    document.getElementById('new-img').setAttribute(
+      'src', img_data_url
+    )
   }
 
   input.addEventListener('change', () => {
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,31 @@
-use std::fmt::format;
-
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
-use base64::{encode,decode};
+use base64::{ encode, decode };
 use image::load_from_memory;
-use image::ImageFormat::Png;
+use image::ImageOutputFormat::Png;
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) -> String {
- log(&"Grayscale Called".into());
+  log(&"Grayscale called".into());
 
- let base64_to_vector=decode(encoded_file).unwrap();
+  let base64_to_vector = decode(encoded_file).unwrap();
+  log(&"Image decoded".into());
 
- let mut img=load_from_memory(&base64_to_vector).unwrap();
+  let mut img = load_from_memory(&base64_to_vector).unwrap();
+  log(&"Image loaded".into());
 
- img=img.grayscale();
+  img = img.grayscale();
+  log(&"Grayscale effect applied".into());
 
- let mut buffer=vec![];
+  let mut buffer = vec![];
+  img.write_to(&mut buffer, Png).unwrap();
+  log(&"New image written".into());
 
-img.write_to(&mut buffer, Png).unwrap();
+  let encoded_img = encode(&buffer);
+  let data_url = format!(
+    "data:image/png;base64,{}",
+    encoded_img
+  );
 
-log(&"Grayscale Applied".into());
-
-let encoded_img=encode(&buffer);
-let data_url=format!("data:image/png:base64,{}",
-encoded_img);
-
-data_url
-
-
+  data_url
 }
